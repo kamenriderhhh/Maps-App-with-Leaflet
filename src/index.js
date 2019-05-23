79dad0f820c250c24ebd15ed405c1a5c313d6238
Kero <ngweykean95@student.usm.my>
2019-05-23 22:06:09 +0800

    first cordova app

--- src/index.js
+++ src/index.js
@@ -8,7 +8,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const startApp = () => {
+    ReactDOM.render(<App />, document.getElementById('root'));
+}
+
+// IOS or Android setting
+if (window.cordova) {
+    document.addEventListener('deviceready', startApp, false);
+} else {
+    startApp();
+}
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ www/precache-manifest.bf263befa03855317edb77700ac51cb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce0e16a6676cb2dc569f672b0c91e271",
+    "url": "./index.html"
+  },
+  {
+    "revision": "61854b226f460c2f8e35",
+    "url": "./static/css/2.441e21e3.chunk.css"
+  },
+  {
+    "revision": "fb04e80270574e9bd197",
+    "url": "./static/css/main.ab27fb1e.chunk.css"
+  },
+  {
+    "revision": "61854b226f460c2f8e35",
+    "url": "./static/js/2.3157932c.chunk.js"
+  },
+  {
+    "revision": "fb04e80270574e9bd197",
+    "url": "./static/js/main.54901a7c.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "b0112773bce4c26d7c68c7597cb0f403",
+    "url": "./static/media/pinpoint.b0112773.png"
+  }
+]);
